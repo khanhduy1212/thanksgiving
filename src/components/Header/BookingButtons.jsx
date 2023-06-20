157c7ad98255c30bd2b7e4e999629d5838111772
khanhduy <khanhduy12122000@gmail.com>
2023-06-20 14:04:36 +0700

    push feat/header
    -install bootstrap
    -create folder Header:
    +component Booking
    + component Header
    +component NavList

---
+++ src/components/Header/BookingButtons.jsx
@@ -0,0 +1,12 @@
+export default function BookingButtons() {
+    return (
+      <div>
+        <button className="btn-book-oxley">
+          Book Oxley Now
+        </button>
+        <button className="btn-book-can">
+          Book Cantonment Now
+        </button>
+      </div>
+    );
+  }
---
+++ src/components/Header/NavList.jsx
@@ -0,0 +1,11 @@
+export default function NavList() {
+  return (
+    <ul className="nav-list">
+      <li><a href="/">Thinksgiving Oxley</a></li>
+      <li><a href="/">Thanksgiving Cantonment</a></li>
+      <li><a href="/">About Us</a></li>
+      <li><a href="/">Blog</a></li>
+      <li><a href="/">Contact Us</a></li>
+    </ul>
+  );
+}
