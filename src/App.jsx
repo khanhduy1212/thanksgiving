dbdefbc581aa233d3130114afbf41ce32d49d74e
khanhduy <khanhduy12122000@gmail.com>
2023-06-27 09:24:43 +0700

    feat: add icon for ourservice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Header from "./components/Header/Header"
 import BigRoom from "./components/Big-room/BigRoom";
 import SpaciousContent from "./components/Spacious-Service/SpaciousContent";
+import OurService from "./components/Our-service/OurService";
+import Gallery from "./components/Photo-Gallery/Gallery";
+import Resident from "./components/Resident/Resident";
 function App(){
     return(
         <>
             <Header/>
-            <BigRoom/>
-            <SpaciousContent/>
+             <BigRoom/>
+             <SpaciousContent/>
+              <OurService/> 
+              <Gallery/>
+              <Resident/>
 
         </>
     )
